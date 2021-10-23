5a79b2053614d49bccd8e012a11c0f1e37d6f977
jayykal <92835027+jayykal@users.noreply.github.com>
2021-10-23 10:48:49 -0400

    Added function to Account.js

---
+++ client/src/Account.js
@@ -0,0 +1,14 @@
+/*
+page for the creation of user accounts
+*/
+import './App.css';
+import React, {useState} from 'react';
+import Axios from 'axios';
+
+/*
+function for class 
+*/
+function Account() {
+
+}
+
