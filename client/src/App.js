9698fc3ae9d35875b537c20d425a8008b7f64cf5
jayykal <92835027+jayykal@users.noreply.github.com>
2021-11-13 13:12:50 -0500

    New cards for restaurants

--- client/src/App.js
+++ client/src/App.js
@@ -13,6 +13,7 @@ import SignUp from "./components/Pages/Sign-Up"
 import Help from "./components/Pages/Help"
 import LoginPage from "./components/Pages/LoginPage"
 import HomeSignedIn from "./components/Pages/HomeSignedIn"
+import Review from "./components/Pages/Review"
 
 function App(){
 
@@ -27,6 +28,7 @@ function App(){
             <Route path="/sign-up" exact component={SignUp} />
             <Route path="/login" exact component={LoginPage} />
             <Route path="/home-signed-in" exact component={HomeSignedIn} />
+            <Route path="/review" exact component={Review} />
         </Switch>
       </Router> 
 
--- client/src/components/PlaceDetails/PlaceDetails.jsx
+++ client/src/components/PlaceDetails/PlaceDetails.jsx
@@ -1,15 +1,60 @@
 import React from 'react';
 import {Rating} from '@material-ui/lab';
+import {Box, Typography, Button, Card, CardMedia, CardContent, CardActions, Chip} from '@material-ui/core';
+import LocationOnIcon from '@material-ui/icons/LocationOn';
+import PhoneIcon from '@material-ui/icons/Phone';
+//import Rating from '@material-ui/lab/Rating';
+import useStyles from './styles';
+import { Link } from 'react-router-dom'
 
 
 const PlaceDetails = ({restaurant, selected, refProp}) => {
     if (selected) refProp?.current?.scrollIntoView({behaviour: "smooth", block: "start"})
-
+    const classes = useStyles();
     return (
-        <>
-        <h3>{restaurant.name}</h3>
-        <h6>{restaurant.rating}</h6>
-        </>
+        <Card elevation={6}>
+            <CardMedia
+                style={{height: 100}}
+                title={restaurant.name}
+            />
+            <CardContent>
+                <Typography gutterBottom variant='h5'>{restaurant.name}</Typography>
+                <Box display='flex' justifyContent='space-between'>
+                    <Typography variant='subtitle1'>Price</Typography>
+                    <Typography gutterBottom variant='subtitle1'>{restaurant.price_level}</Typography>
+                </Box>
+                <Box display='flex' justifyContent='space-between'>
+                    <Typography variant='subtitle1'>Ranking</Typography>
+                    <Typography gutterBottom variant='subtitle1'>{restaurant.ranking}</Typography>
+                </Box>
+                {restaurant?.cuisine?.map(({name}) => (
+                    <Chip key={name} size="small" label={name} className={classes.chip}/>
+                ))}
+                {restaurant?.address && (
+                    <Typography gutterBottom variant="subtitle2" color="textSecondary" className={classes.subtitle}>
+                        <LocationOnIcon /> {restaurant.address}
+                    </Typography>
+                )}
+                {restaurant?.phone && (
+                    <Typography gutterBottom variant="subtitle2" color="textSecondary" className={classes.spacing}>
+                        <PhoneIcon /> {restaurant.phone}
+                    </Typography>
+                )}
+                <CardActions>
+                    <Button size="small" color="primary"> 
+                        <Link
+                            to='/review'
+                        >
+                         Leave a Review   
+                        </Link>
+                        
+                    </Button>
+                    <Button size="small" color="primary" onClick={() => window.open(restaurant.website, '_blank')}>
+                        Website
+                    </Button>
+                </CardActions>
+            </CardContent>    
+        </Card>
     );
 }
 
