16c1842d3567c9747f96ca4856e68af044d4a596
rdoughty10 <rdoughty20@sjs.org>
2021-11-29 15:34:07 -0500

    restaurant reviews page

---
+++ client/src/components/Pages/RestaurantReviews.jsx
@@ -0,0 +1,81 @@
+import React, { useState, useEffect }  from 'react';
+import { CssBaseline, Grid, Typography } from '@material-ui/core';
+import Axios from 'axios';
+
+import Header from '../Header/Header';
+import 'regenerator-runtime/runtime'
+
+const MyReviews = () => {
+
+  const [bounds, setBounds] = useState({});
+  const [childClicked, setChildClicked] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const [user, setUser] = useState({});
+  const [loggedIn, setLoggedIn] = useState(false)
+
+  const [reviews, setReviews] = useState({});
+
+
+
+  useEffect(() => {
+    Axios.get('http://localhost:3001/api/user/get').then((response) => {
+      console.log(response)
+      if (response.data.loggedIn == true){
+        Axios.post('http://localhost:3001/api/reviews/user', {
+            userID: response.data.user[0].id,
+        }).then((response) => {
+            console.log(response)
+            setReviews(response.data)
+        })
+        setUser(response.data.user[0])
+        setLoggedIn(true)
+      }else{
+        setUser({})
+        setLoggedIn(false)
+      }
+    });
+  }, [])
+
+  if(loggedIn) {
+    return (
+        <>
+        <CssBaseline />
+        <Header />
+        <h1> {user.firstName + " " + user.lastName}'s Reviews</h1>
+        <Grid container spacing={3} style={{width: '100%'}}>
+            {Array.from(reviews)?.map((review, i) => (
+                <Grid item xs={12} md={12}>
+                    <h2> {review.restaurantName}</h2>
+                    <h4> {review.rating}/5</h4>
+                    <p> {review.review}</p>
+
+                </Grid>  
+            ))}
+                     
+        </Grid>
+        </>   
+    );
+    } else {
+        return (
+            <>
+        <CssBaseline />
+        <Header />
+        <Grid container spacing={3} style={{width: '100%'}}>
+            <Grid item xs={12} md={12}>
+                <Typography variant="h4">
+                    Please login to access your reviews.
+                </Typography>
+            </Grid> 
+            <Grid item xs={12} md={12}>
+                <Typography variant="h5">
+                    The login button is in the top right of the screen.
+                </Typography>
+            </Grid>          
+        </Grid>
+        </>       
+    );
+    }
+} 
+
+export default MyReviews
